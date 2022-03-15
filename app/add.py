41662859f253d2ce05f77753e07b707b7a0e9af7
Robert Lützner <robert.luetzner@pm.me>
2022-03-15 14:35:16 +0100

    Add missing 'days' option to 'add' subcommand

--- app/add.py
+++ app/add.py
@@ -6,13 +6,20 @@ from datetime import date
 
 @click.command()
 @click.option('--name', help='The name of the person you want to stay in touch with', required=True)
-def add(name):
+@click.option('--days', help='How many days between reminders')
+def add(name, days):
     d = data.load()
     if any(filter(lambda f: f.name == name, d.friends)):
         raise click.UsageError(f'{name} is already in the reminder list.')
     c = config.load_config()
     today = date.today()
     day = today.strftime('%Y-%m-%d')
-    d.friends.append(data.friend(name, c.default_days, day))
+    if not days:
+        days = c.default_days
+    try:
+        days = int(days)
+    except:
+        raise click.UsageError(f'{days} is not a valid number.')
+    d.friends.append(data.friend(name, days, day))
     d.save()
     print(f'Added {name} succesfully.')
